77d73987ca24421b191075808e0e1bb220855186
MurituMaina <vincent.maina@student.moringaschool.com>
2022-09-02 10:27:18 +0300

    update changes on the timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -149,36 +149,26 @@ versesbg.backgroundimage = "url('https://unsplash.com/photos/yC-Yzbqy7PY')"
 
 
 ///TIMER
-    
-
-
 let countDownDate = new Date("Sep 7, 2022 05:00:00").getTime();
-
-// Update the count down every 1 second
-window.addEventListener('DOMContentLoaded', x)
-let x = setInterval(function() {
-
-  // Get today's date and time
+window.addEventListener('DOMContentLoaded', () => console.log("I am here"))
+let wait = setInterval(function() {
   let now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
   let distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
   let days = Math.floor(distance / (1000 * 60 * 60 * 24));
   let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="timer"
  let timer =  document.getElementById("timer")
  timer.innerText = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
     console.log(days + "d " + hours + "h "
     + minutes + "m " + seconds + "s ");
-  // If the count down is over, write some text 
   if (distance < 0) {
-    clearInterval(x);
+    clearInterval(wait);
     document.getElementById("timer").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+
+
+// hbbhh
