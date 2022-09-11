1dfc0390de6072bc8456ae5cb34edb7d86680728
MurituMaina <vincent.maina@student.moringaschool.com>
2022-09-11 23:50:53 +0300

    auto update the timer with user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,24 @@
 // For Verse HTML
-window.addEventListener('DOMContentLoaded', versesRead);
-window.addEventListener('DOMContentLoaded', dailyVerse);
-window.addEventListener('DOMContentLoaded', didYou);
-window.addEventListener('DOMContentLoaded', setReminder);
+// window.addEventListener('DOMContentLoaded', readBible);
+// window.addEventListener('DOMContentLoaded', dailyVerse);
+// window.addEventListener('DOMContentLoaded', didYou);
+// window.addEventListener('DOMContentLoaded', setReminder);
+// window.addEventListener('DOMContentLoaded', chapterChanger);
+// window.addEventListener('DOMContentLoaded', sources);
 
-// window.addEventListener('DOMContentLoaded',() =>{
-//   versesRead();
-//   dailyVerse();
-//   didYou();
-//   setReminder(date, time);
-// } );
 
-function versesRead( books = 'Genesis', chapter ,verse =1){
-    // https://bible-api.com/john%203:16?verse_numbers=true
+window.addEventListener('DOMContentLoaded', () =>{
+readBible();
+ dailyVerse();
+ didYou();
+ setReminder(event, date, time);
+ chapterChanger();
+ sources();
+
+});
+
+
+function readBible( books = 'Genesis', chapter ,verse=1){
     fetch(`https://bible-api.com/Genesis${chapter}:1-${verse}?verse_numbers=true`)
     .then((resp) => resp.json())
     .then((data) => {
@@ -20,9 +26,18 @@ function versesRead( books = 'Genesis', chapter ,verse =1){
     scripture.innerHTML = `<h4 class="main_header">${data.reference}</h4>
     <p class="main_verses">${data.text}</p>`
     })
-  .catch((error) => console.log(error + "Error!!"))
+  // .catch((error) => console.log(error + "Error!!"))
 }
-
+// function versesRead( books = 'Genesis', chapter ,verse =1){
+//   fetch(`https://bible-api.com/Genesis${chapter}:1-${verse}?verse_numbers=true`)
+//   .then((resp) => resp.json())
+//   .then((data) => {
+//   let scripture = document.getElementById("versesToRead")
+//   scripture.innerHTML = `<h4 class="main_header">${data.reference}</h4>
+//   <p class="main_verses">${data.text}</p>`
+//   })
+// .catch((error) => console.log(error + "Error!!"))
+// }
 let book = ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua",
  "Judges", "Ruth", "1 Samuel", "2 Samuel", "1 Kings", "2 Kings", "1 Chronicles",
   "2 Chronicles", "Ezra", "Nehemiah", "Esther", "Job", "Psalms", "Proverbs", "Ecclesiastes", "Song of Solomon", "Isaiah", "Jeremiah", "Lamentations", "Ezekiel", "Daniel", "Hosea", "Joel", "Amos", "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zephaniah", "Haggai", "Zechariah", "Malachi", "Matthew", "Mark", "Luke", "John", "Acts", "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians", "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy", "2 Timothy", "Titus", "Philemon", "Hebrews", "James", "1 Peter", "2 Peter", "1 John", "2 John", "3 John", "Jude", "Revelation"];
@@ -30,18 +45,61 @@ let book = ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua"
 const booksChapters ={ Genesis: 50,  Exodus: 40,  Leviticus: 27, Numbers: 36, Deuteronomy: 34,  Joshua: 24,  Judges: 21,  Ruth: 4,  "1 Samuel": 31,  "2 Samuel": 24,  "1 Kings": 22,  "2 Kings": 25,  "1 Chronicles": 29,  "2 Chronicles": 36,  Ezra: 10,  Nehemiah: 13,  Esther: 10,  Job: 42,  Psalms: 150,  Proverbs: 31,  Ecclesiastes: 12,  "Song of Solomon": 8,  Isaiah: 66,  Jeremiah: 52,  Lamentations: 5,  Ezekiel: 48,  Daniel: 12,  Hosea: 14,  Joel: 3,  Amos: 9,  Obadiah: 1,  Jonah: 4,  Micah: 7,  Nahum: 3,  Habakkuk : 3,  Zephaniah: 3,  Haggai: 2,  Zechariah: 14,  Malachi: 4,   Matthew:28,  Mark:16,  Luke:24,  John:21,  Acts:28,  Romans:16,  "1 Corinthians":16,  "2 Corinthians":13,  Galatians:6,  Ephesians:6,  Philippians:4,  Colossians:4,  "1 Thessalonians":5,  "2 Thessalonians":3,  "1 Timothy":6,  "2 Timothy":4,  Titus:3,  Philemon:1,  Hebrews:13,  James:5,  "1 Peter":5,  "2 Peter":3,  "1 John":5,  "2 John":1,  "3 John":1,  Jude:1,  Revelation:22  }
 
 let select = document.getElementById("books");
+select.addEventListener('change', chapterChanger)
 let chapterSelect = document.getElementById("chapter");
 let verseSelect = document.getElementById('verse');
 
-for(var i = 0; i < book.length; i++){
-    select.options.add(new Option(book[i], book[i]));
+// let b;
+// for( b = 0; b < book.length; b++){
+//     select.options.add(new Option(book[b], book[b]));
+//   // if book is gen the 50 chaper
+// }
+function sources(){
+fetch('/assets/js/db.json')
+.then((resp) => resp.json())
+.then((data)=> {
+  // console.log(data.book)
+  for(let b = 0; b < data.book.length; b++ ){
+    select.options.add(new Option(book[b], book[b]));
+
+  }
+})
 }
-for(let i= 0; i < 150; i++){
-chapterSelect.options.add(new Option([i+1], i+1),)
+
+function chapterChanger(){
+  let selectBook = document.getElementById("books").value;
+  console.log(selectBook)
+  let bookChosen = Object.keys(booksChapters);
+  let bookC = Object.entries(booksChapters);
+  for(let choice in bookChosen){
+  if(bookChosen[choice].includes(selectBook) === true){
+    let chapterNum = 0;
+      chapterNum = bookC[choice][1]
+    // console.log(chapterNum + " welcome On board")
+    
+    for(let i= 0; i < chapterNum; i++){
+      // chapterSelect.options.remove();
+      // chapterSelect.options[0]= null;
+      /*
+     
+      */
+  //    if() {const option = new Option([i+1]) ;
+  //  chapterSelect.add(option)
+      chapterSelect.options.add(new Option([i+1]))
+    // }
+    }
+    }
+  }
+  }
+
+let v;
+for(v=0; v < 175; v++){
+verseSelect.options.add(new Option(v+1))
 }
-for(let i =0; i< 175; i++){
-verseSelect.options.add(new Option(i+1))
-}
+
+
+
+
 
 let search = document.getElementById("searchRead");
 search.addEventListener('change', searchRead);
@@ -66,7 +124,7 @@ function verseRead( books = 'Genesis', chapter =1 ,verse=1){
 
   }
 })
-.catch((error) => console.log(error + "Error!!"))
+// .catch((error) => console.log(error + "Error!!"))
 }
 
 
@@ -84,7 +142,7 @@ function dailyVerse (){
           // console.log(data.reference, data.text)
         let daily_verse = document.getElementById("daily_verse");
         daily_verse.innerHTML = `<h3>${data.reference}</h3><h4>${data.translation_name}</h4><p> ${data.text}</p><a href=#readmore>Read More...</a>`})
-        .catch((error) => console.log(error + " daily Error!!"))
+        // .catch((error) => console.log(error + " daily Error!!"))
 
     }
 // End
@@ -145,7 +203,7 @@ let did_youKnow = document.getElementById('did_youKnow')
   did_youKnow.innerHTML = `<h4 id="quote_head">${data.reference}</h4>
    <p id="quote_verses">${data.text}</p>`
    })
- .catch((error) => console.log(error + "did Error!!"))
+//  .catch((error) => console.log(error + "did Error!!"))
 }
 
 
@@ -153,50 +211,44 @@ let did_youKnow = document.getElementById('did_youKnow')
 // Collapsible button
 let coll = document.getElementsByClassName("collapsible");
 
-// for (let i = 0; i < coll.length; i++) {
   coll.addEventListener("click", function() {
     coll.classList.toggle("active");
     let content = document.getElementById("setReminderTime")
     if (content.style.display === "none") {
-      // console.log("hello Kenya")
       content.style.display = "block";
     } else {
       content.style.display = "block";
     }
   });
-// }
 
-
-//function to set reminder
-// let dateInput=document.getElementById('date');
-// let date = dateInput.target.value;
-// let timeInput = document.getElementById('time');
-// let time = timeInput.target.value;
-// console.log(date, time)
 
 let reminder = document.getElementById('setter');
- reminder.addEventListener("click", starter)
+ reminder.addEventListener("submit", starter)
+ function dateBegin(){
+  starter(event)
+ }
 
- const starter =  (event)=> {
+ function starter(event){
   event.preventDefault();
   let dateInput=document.getElementById('date').value;  
-  alert(dateInput);
+  let timeInput = document.getElementById('time').value;
   console.log("date:" + dateInput)
-  let timeInput = document.getElementById('time');
-  let time = timeInput.target.value;
-  console.log("Got this"+dateInput, time)
-
-  setReminder(event, date, time)
+   timeInput =timeInput.toString();
+  console.log("Got this"+dateInput, timeInput)
+  setReminder(event, dateInput, timeInput)
 }
 
 
 function setReminder(event,
-  date='01-25-2023', time= '05:00'){
+  date ='01-25-2023', time = '05:00'){
   event.preventDefault();
   const [m, d, y] = date.split('-');
   let setTime = [m,d,y,time];
+  console.log(setTime)
   setTime =setTime.join(" ");
-//TIMER
+  console.log(setTime)
+
+  //TIMER
 const yearDate = new Date(`${setTime}`).getTime();
 let x = setInterval( function ()
 {
@@ -213,7 +265,4 @@ let x = setInterval( function ()
         document.getElementById("timer").innerHTML = "EXPIRED";
       }
 }, 1000)
-
-
-
 }
